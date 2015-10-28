421a1b05b948bcd00a8498d3384ccd3c9c4b3485
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-10-28 14:23:16 +0100

    Mount routes on /cadastre

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'short' : 'dev'));
 
 /* Routes */
-app.use('/', require('./routes')({
+app.use('/cadastre', require('./routes')({
     key: process.env.GEOPORTAIL_KEY,
     referer: process.env.GEOPORTAIL_REFERER || 'http://localhost'
 }));
